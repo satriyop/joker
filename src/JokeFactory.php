bbe5adee898bcc3c86ec1b5233a74c10b371db9e
Satriyo Pamungkas <satriyopamungkas@gmail.com>
2020-11-26 03:39:58 +0700

    Initial commit

---
+++ src/JokeFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Satriyop\Joker;
+
+class JokeFactory
+{
+    protected $jokes = [
+        "Web Developer keep everything important in their <head>",
+        "Why MongoDB developer do not have a date ? Because they dont understand relationship"
+    ];
+
+    public function __construct(array $jokes = null)
+    {
+        if ($jokes) {
+            $this->jokes = $jokes;
+        } 
+    }
+
+    public function getRandomJoke()
+    {
+        return $this->jokes[array_rand($this->jokes)];    
+    }
+}
---
+++ tests/JokeFactoryTests.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Satriyop\Joker\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Satriyop\Joker\JokeFactory;
+
+class JokeFactoryTests extends TestCase
+{
+    /** @test */
+    public function test_create_random_jokes()
+    {
+        $jokes = new JokeFactory([
+            "This is a joke"
+        ]);
+        $joke = $jokes->getRandomJoke();
+
+        $this->assertSame("This is a joke", $joke);
+    }
+
+
+    /** @test */
+    public function it_return_predefined_joke()
+    {
+        $jokes = new JokeFactory();
+        $joke = $jokes->getRandomJoke();
+        
+        $predefinedJokes = [
+            "Web Developer keep everything important in their <head>",
+            "Why MongoDB developer do not have a date ? Because they dont understand relationship"
+        ];
+
+        $this->assertContains($joke, $predefinedJokes);
+    }
+}
